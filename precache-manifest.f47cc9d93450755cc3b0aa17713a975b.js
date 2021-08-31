d453da66cf94d48df8687e0ae4a2d89c7d707b99
Cesar Julian Salazar <trabajos.cesar23@gmail.com>
2021-08-31 00:50:39 -0500

    Updates

---
+++ precache-manifest.f47cc9d93450755cc3b0aa17713a975b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "dde4e7536801befc5c9b",
+    "url": "/react-proyecto-platzi/static/css/main.c511601f.chunk.css"
+  },
+  {
+    "revision": "dde4e7536801befc5c9b",
+    "url": "/react-proyecto-platzi/static/js/main.dde4e753.chunk.js"
+  },
+  {
+    "revision": "68ac6811e9db36a16d87",
+    "url": "/react-proyecto-platzi/static/js/runtime~main.68ac6811.js"
+  },
+  {
+    "revision": "5acb88f72e0b6842bedc",
+    "url": "/react-proyecto-platzi/static/css/2.acea06bf.chunk.css"
+  },
+  {
+    "revision": "5acb88f72e0b6842bedc",
+    "url": "/react-proyecto-platzi/static/js/2.5acb88f7.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/react-proyecto-platzi/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/react-proyecto-platzi/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/react-proyecto-platzi/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/react-proyecto-platzi/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/react-proyecto-platzi/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "d5531f35ee338a30561a74658f88f8a8",
+    "url": "/react-proyecto-platzi/index.html"
+  }
+];
